bc57904d22fbd51f0a5c6126d5dee4354c9a60e9
ryscet <cetnarski.ryszard@gmail.com>
2016-05-15 17:29:09 +0200

    rtd

--- docs/conf.py
+++ docs/conf.py
@@ -133,8 +133,8 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0,"/Users/ryszardcetnarski/pySeries")
+#sys.path.insert(0, os.path.abspath('../..'))
+#sys.path.insert(0,"/Users/ryszardcetnarski/pySeries")
 
 # -- General configuration ------------------------------------------------
 
