2083f689eb99723df55e16b6124cb14b3295d135
ryscet <cetnarski.ryszard@gmail.com>
2016-06-08 17:34:45 +0200

    rtd

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,9 @@ import os
 # ones.
 
 sys.path.insert(0, os.path.abspath('..'))
-
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../'))
+import sphinx_bootstrap_theme
 
 extensions = [
     'sphinx.ext.autodoc',
