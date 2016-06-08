2aad4be432871cbc7b9fd0a3fd8250404ae28b9d
ryscet <cetnarski.ryszard@gmail.com>
2016-06-08 16:06:05 +0200

    rtd

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,9 @@ import os
 # ones.
 
 sys.path.insert(0, os.path.abspath('../..'))
-
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, '/Users/user/Desktop/repo_for_pyseries/pyseries/')
+sys.path.insert(0, '/Users/user/Desktop/repo_for_pyseries/pyseries/pyseries')
 
 extensions = [
     'sphinx.ext.autodoc',
